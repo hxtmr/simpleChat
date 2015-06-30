37a4ae24b474fcbb29fa6bfc883389044e2e783e
maxinabin <624826172@qq.com>
2015-06-30 13:28:32 +0800

    update config/simpleChat-config.js

--- config/simpleChat-config.js
+++ config/simpleChat-config.js
@@ -1,21 +1,4 @@
 module.exports = {
     host: '',
     port: 80
-    /*db: {
-        user: "lancet-ane",
-        pass: "lancet-ane@dfsoft",
-        host: "192.168.0.33",
-        port: "3306",
-        database: "lancet-ane",
-        charset: "UTF8_GENERAL_CI",
-        showsql: false,
-        waitforconnections: true, //超过最大连接数后是否等待
-        connectionlimit: 99, //最大数据库连接个数，需要小于数据库max_connections 个数。 SHOW GLOBAL VARIABLES LIKE 'max_connections';
-        queuelimit: 100 //等待的任务数
-    },A
-    hummerurl: "http://oa.dfsoft.com.cn:3030/hummer",
-    templates: "standard", // standard: 标准文书    henan: 河南省标准   zzsdyrmyy: 郑州市第一人民医院 zzszxyy: 郑州市中心医院
-    mode: "full", // full: 完整版   lite: 定制版    standard: 标准版
-    version: '1.0.24', // 版本号，在开始菜单中显示
-    logo: "dcsoft" //none: 没有logo的图片路径 dcsoft: 大成的logo路径 */
 };
